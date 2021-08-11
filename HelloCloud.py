b57aa26eddf5c152cb4f6ccecf78a032c4d626f8
thanapongsakorn <thanaarppp@gmail.com>
2021-08-11 11:13:46 +0700

    Modify

--- HelloCloud.py
+++ HelloCloud.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello Thanapongsakorn RUK-COM!</h1>'
+    return '<h1>Hello Thanapongsakorn RUK-COM! Great Clould Services</h1>'
 
 
 @app.route('/home',methods=['GET','POST'])
